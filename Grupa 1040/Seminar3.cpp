ca19f64a6520338c7ebfdbc8992cdfa138031412
irinaenachescu <irina.enachescu@csie.ase.ro>
2017-10-19 23:36:37 +0300

    Third Lab

---
+++ Grupa 1040/Seminar3.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+
+using namespace std;
+
+class Apartament {
+private:
+	char* proprietar;
+	char adresa[50];
+	unsigned int an_constructie;
+	char confort;
+	int numar_camere;
+	double* suprafete;
+
+public:
+	Apartament() {
+		this->proprietar = new char[strlen("Anonim") + 1];
+		strcpy(this->proprietar, "Anonim");
+		strcpy(this->adresa, "Necunoscuta");
+		this->an_constructie = 2000;
+		this->confort = '1';
+		this->numar_camere = 0;
+		this->suprafete = NULL;
+	}
+	void afisare() {
+		cout << "Proprietar: " << this->proprietar << endl;
+		cout << "Adresa: " << this->adresa << endl;
+		cout << "An constructie: " << this->an_constructie << endl;
+		cout << "Confort: " << this->confort << endl;
+		cout << "Numar camere: " << this->numar_camere << endl;
+		for (int i = 0; i < this->numar_camere; i++) {
+			cout << "Suprafata camerei " << i + 1 << " este:" << this->suprafete[i] << endl;
+		}
+	}
+
+	//destructor
+	~Apartament() {
+		cout << "Destructor....." << endl;
+		if (this->proprietar != NULL) {
+			delete[] this->proprietar;
+		}
+		if (this->suprafete != NULL) {
+			delete[] this->suprafete;
+		}
+	}
+};
+
+void main() {
+	Apartament a1;
+	a1.afisare();
+
+	//un pointer la un obiect existent
+	Apartament* pa1;
+	pa1 = &a1;
+	pa1->afisare();
+
+	Apartament* pa2 = new Apartament();
+	pa2->afisare();
+	delete pa2;
+}
